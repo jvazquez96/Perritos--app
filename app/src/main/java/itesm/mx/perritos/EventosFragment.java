7e9e4cbe2a6f535967dbc2b0afb38ebea7300402
jvazquez96 <jorgevzqz6@gmail.com>
2017-03-21 15:24:03 -0600

    Fix some weird issues

--- app/src/main/java/itesm/mx/perritos/EventosFragment.java
+++ app/src/main/java/itesm/mx/perritos/EventosFragment.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-class EventosFragment extends ListFragment {
+public class EventosFragment extends ListFragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
