bd56a55b567a80c5cf4826963056bc09995035d0
Alexandro Trujillo <A01328426@itesm.mx>
2017-03-26 13:32:46 -0600

    last commit , errores de xml

--- app/src/main/java/itesm/mx/perritos/MainActivity.java
+++ app/src/main/java/itesm/mx/perritos/MainActivity.java
@@ -1,22 +1,31 @@
 package itesm.mx.perritos;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSelectedListener, PetFragment.OnPetSelectedListener, View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSelectedListener, PetFragment.OnPetSelectedListener, View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
 
     private TabLayout tlTabLayout;
     private ViewPager vpViewPager;
@@ -30,6 +39,31 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //******SECTION ADDED
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        /* SECTION FOR FLOATING BUTTON
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+ */
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //******SECTION ADDED END
+
+
         vpViewPager = (ViewPager) findViewById(R.id.viewpager);
         setUpViewPager(vpViewPager);
 
@@ -37,13 +71,13 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
         tlTabLayout.setupWithViewPager(vpViewPager);
         tlTabLayout.addOnTabSelectedListener(this);
 
-        // Custom toolbar
+        // Custom toolbar  EMLIMINAR ESTA PARTE  PUES NO PERMITE FUNCIONAR EL FLOATBUTTON
         tbToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(tbToolbar);
 
-        // Buttons from the toolbar
-        imgbtnMenu = (ImageButton) tbToolbar.findViewById(R.id.button_menu);
-        imgbtnMenu.setOnClickListener(this);
+        // Buttons from the toolbar ************************ERROR***********************************
+        //imgbtnMenu = (ImageButton) tbToolbar.findViewById(R.id.button_menu);
+        //imgbtnMenu.setOnClickListener(this);
 
     }
 
@@ -55,8 +89,6 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
                 Log.d(DEBUG_TAG,"MENU Button");
                 break;
         }
-
-
     }
 
     @Override
@@ -120,5 +152,65 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
         public CharSequence getPageTitle(int position) {
             return mFragmentTitleList.get(position);
         }
+    }
+
+    // ************ADDED
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+       } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+
     }
 }
