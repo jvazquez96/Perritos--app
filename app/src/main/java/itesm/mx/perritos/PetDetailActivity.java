3cdb45f2b7595d21113adc79b55c2b66bd3159bd
jvazquez96 <jorgevzqz6@gmail.com>
2017-03-28 13:36:33 -0600

    Fix something

--- app/src/main/java/itesm/mx/perritos/PetDetailActivity.java
+++ app/src/main/java/itesm/mx/perritos/PetDetailActivity.java
@@ -21,7 +21,7 @@ public class PetDetailActivity extends AppCompatActivity {
         ivPet = (ImageView) findViewById(R.id.image_pet);
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
-            Pet pet = (Pet) bundle.getSerializable("Pet");
+            Pet pet = (Pet)c bundle.getSerializable("Pet");
             Log.d("DEBUG_TAG","Pet image: " + pet.getIdImage());
 //            ivPet.setImageResource(pet.getIdImage());
         }
