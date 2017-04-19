c29459af93cb94e755a3d72b03dcf75591f9fa97
Alexandro Trujillo <A01328426@itesm.mx>
2017-04-19 14:30:07 -0500

    Detail items, email (request) and maps(dirrections)  buttons  added

--- app/src/main/java/itesm/mx/perritos/pet/PetDetailActivity.java
+++ app/src/main/java/itesm/mx/perritos/pet/PetDetailActivity.java
@@ -1,13 +1,18 @@
 package itesm.mx.perritos.pet;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
@@ -17,15 +22,14 @@ import java.util.Calendar;
 import itesm.mx.perritos.R;
 
 
-public class PetDetailActivity extends AppCompatActivity {
+public class PetDetailActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ImageView ivPet;
     private TextView tvName;
     private TextView tvDescription;
     private TextView tvDate;
-
+    private Button btnSolicitudAdopcion;
     private Toolbar tlToolbar;
-
     private Calendar calendar;
 
     @Override
@@ -37,9 +41,12 @@ public class PetDetailActivity extends AppCompatActivity {
         tvDescription = (TextView) findViewById(R.id.text_description);
         tvDate = (TextView) findViewById(R.id.text_date);
         tvDescription = (TextView) findViewById(R.id.text_description);
+        btnSolicitudAdopcion = (Button) findViewById(R.id.enviar_email_btn);
         Bundle bundle = getIntent().getExtras();
         calendar = Calendar.getInstance();
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd MM yyyy");
+        btnSolicitudAdopcion.setOnClickListener(this);
+
 
         if (bundle != null) {
             Pet pet = (Pet) bundle.getSerializable("Pet");
@@ -52,6 +59,7 @@ public class PetDetailActivity extends AppCompatActivity {
             tvDescription.setText(pet.getDescription());
             tvDate.setText(simpleDateFormat.format(calendar.getTime()));
         }
+
 
         tlToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(tlToolbar);
@@ -73,5 +81,39 @@ public class PetDetailActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.detail,menu);
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.enviar_email_btn:
+                enviarMail();
+                break;
+        }
+
+    }
+
+    public void enviarMail(){
+        String TO [] = {"nobody@mail.com"};
+        //String CC [] = {"nobody@mail.com"};
+
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setData(Uri.EMPTY.parse("mailto:"));
+        emailIntent.setType("text/plain");
+
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        //emailIntent.putExtra(Intent.EXTRA_CC, CC);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Solicitud de adopción");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
+
+        try{
+            startActivity(Intent.createChooser(emailIntent,"Solicitud de adopción por correo..."));
+            finish();
+        }catch (android.content.ActivityNotFoundException ex){
+            Toast.makeText(PetDetailActivity.this,
+                    "No existe algún cliente de correo electronico en el dispositivo.", Toast.LENGTH_SHORT).show();
+        }
+
+
     }
 }
--- app/src/main/java/itesm/mx/perritos/store/ProductDetailActivity.java
+++ app/src/main/java/itesm/mx/perritos/store/ProductDetailActivity.java
@@ -1,14 +1,21 @@
 package itesm.mx.perritos.store;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import itesm.mx.perritos.R;
+import itesm.mx.perritos.pet.PetDetailActivity;
 
-public class ProductDetailActivity extends AppCompatActivity {
+public class ProductDetailActivity extends AppCompatActivity implements View.OnClickListener{
     private Toolbar toolbar;
+    private Button btnSolicitudProducto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +26,49 @@ public class ProductDetailActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Producto");
+
+        btnSolicitudProducto = (Button) findViewById(R.id.btn_solicitud_producto);
+
+        btnSolicitudProducto.setOnClickListener(this);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.detail, menu);
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_solicitud_producto:
+                enviarMail();
+                break;
+        }
+
+    }
+
+    public void enviarMail(){
+        String TO [] = {"nobody@mail.com"};
+        //String CC [] = {"nobody@mail.com"};
+
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setData(Uri.EMPTY.parse("mailto:"));
+        emailIntent.setType("text/plain");
+
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        //emailIntent.putExtra(Intent.EXTRA_CC, CC);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Solicitud de producto");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
+
+        try{
+            startActivity(Intent.createChooser(emailIntent,"Solicitud de producto por correo..."));
+            finish();
+        }catch (android.content.ActivityNotFoundException ex){
+            Toast.makeText(ProductDetailActivity.this,
+                    "No existe algún cliente de correo electronico en el dispositivo.", Toast.LENGTH_SHORT).show();
+        }
+
+
     }
 }
